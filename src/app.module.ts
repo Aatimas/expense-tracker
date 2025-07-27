64591c5aeffbc388e99ee3d6bcc0d50f397f5467
Anish Shahi <anishshahi012@gmail.com>
2025-07-27 21:31:59 +0545

    connect to db using config and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: '2077',
-      username: 'postgres',
-      entities: [],
-      database: 'expense-tracker',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
       synchronize: true,
-      logging: true,
+      logging:true,
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   password: '2077',
+    //   username: 'postgres',
+    //   entities: [],
+    //   database: 'expense_tracker',
+    //   synchronize: true,
+    //   logging: true,
+    // }),
   ],
 })
 export class AppModule {}
